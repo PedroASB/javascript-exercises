942d0f53a05f11e3645ab501167565401213a769
Pedro Belo <79765850+PedroASB@users.noreply.github.com>
2025-05-26 19:31:12 -0300

    feat: solve exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let rep = '';
+    while (num--) rep += string;
+    return rep;
 };
 
 // Do not edit below this line
