a8b9d529ffbe49f16d1b93b5b2a9b1f5a9ad6d48
Pedro Belo <79765850+PedroASB@users.noreply.github.com>
2025-05-26 22:00:41 -0300

    feat: solve exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || 
+        !Number.isInteger(b) || 
+        a < 0 || b < 0) {
+        return "ERROR";
+    }
+    if (a > b) {
+        let tmp = a;
+        a = b;
+        b = tmp;
+    }
+    n = b - a + 1;
+    return (a + b) * (n / 2);
 
 };
 
