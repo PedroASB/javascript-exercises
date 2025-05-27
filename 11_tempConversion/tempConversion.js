fe92bf41de4af88b43e2ac159a541ebe6ad30a44
Pedro Belo <79765850+PedroASB@users.noreply.github.com>
2025-05-26 22:17:38 -0300

    feat: solve exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celsius = (fahrenheit - 32) * (5 / 9);
+  return Number.isInteger(celsius) ? celsius : 
+          Number.parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  fahrenheit = celsius * (9 / 5) + 32;
+  return Number.isInteger(fahrenheit) ? fahrenheit :
+          Number.parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
