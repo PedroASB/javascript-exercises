9ca062317706f3792bec574027be6ee448fefb95
Pedro Augusto <79765850+PedroASB@users.noreply.github.com>
2025-06-02 14:35:42 -0300

    feat: solve exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let currentYear = (new Date()).getFullYear();
+    let oldest, greatestAge = 0;
+    array.forEach((person) => {
+        let age = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        if (age > greatestAge) {
+            oldest = person;
+            greatestAge = age;
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
