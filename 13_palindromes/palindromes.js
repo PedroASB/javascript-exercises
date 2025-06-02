5753341d02077d6a7627cd33a4b66b38976a184a
Pedro Augusto <79765850+PedroASB@users.noreply.github.com>
2025-06-02 13:50:24 -0300

    feat: solve exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+    str = str.toLowerCase().replaceAll(/[^a-z0-9]/g, "");
+    for (let i = 0, j = str.length - 1; i < str.length; i++, j--) {
+        if (str[i] !== str[j]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
