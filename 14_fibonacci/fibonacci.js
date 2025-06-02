d1fd04e6ab1d127843d0c570f8025ae953fd7b9e
Pedro Augusto <79765850+PedroASB@users.noreply.github.com>
2025-06-02 14:03:09 -0300

    feat: solve exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 0) return 0;
+    if (num == 1) return 1;
+    if (num < 0) return "OOPS";
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
+
+for (let i = 0; i < 10; i++) {
+    console.log(i, fibonacci(i));
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
